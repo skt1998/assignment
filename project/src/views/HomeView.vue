d2a93a98a1ec7da85f126c76e7b9aa24a2957304
SURAJ KUMAR <sktivari1@gmail.com>
2023-10-22 15:32:12 +0530

    project created

---
+++ project/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <helloWorld />
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
