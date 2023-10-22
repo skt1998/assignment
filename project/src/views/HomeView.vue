26e352cddf588d4d6981dad5736796332e37bf87
SURAJ KUMAR <sktivari1@gmail.com>
2023-10-22 15:53:04 +0530

    card

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,15 +1,28 @@
 <template>
-  <helloWorld />
+<div>
+  <v-card width="250px" height="300px" class="" outlined> 
+      <v-card-subtitle class="px-0 pt-0 pb-1">Title</v-card-subtitle>
+      <v-card-Title class="px-0 pt-0 pb-1">Title</v-card-title>
+         <v-card-subtitle class=" px-0 pt-0 pb-1">Project Techonology</v-card-subtitle>
+         <span v-for="(i,index) in 5" :key="index">java</span>
+         <v-card-subtitle class=" px-0 pt-0 pb-1">Techinical_skill_backend</v-card-subtitle>
+         <span v-for="(i,index) in 5" :key="index">java</span>
+         <v-card-subtitle class=" px-0 pt-0 pb-1">Techinical_skill_Backend</v-card-subtitle>
+         <span v-for="(i,index) in 5" :key="index">java</span>
+         <v-card-subtitle class="px-0 pt-0 pb-1">Techinical_skill_Backend</v-card-subtitle>
+         <span v-for="(i,index) in 5" :key="index">java</span>      
+      
+      </v-card>
+</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+ 
+
 
   export default {
     name: 'HomeView',
 
-    components: {
-      HelloWorld,
-    },
+
   }
 </script>
